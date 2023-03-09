b9c1297e6d0b8442165ecafa2a6433c686bc6e41
Akash <akashsunilsingh5555@gmail.com>
2023-03-09 20:12:49 +0530

    Update settings.py
    add this

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'studentstudyportal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'notesplus',
-        'HOST':'localhost',
-        'PORT':'3306',
-        'USER':'root',
-        'PASSWORD':'root123',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
