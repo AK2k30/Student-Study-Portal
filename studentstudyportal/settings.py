8aceed023b548a9328c480e7a22589fe4c806eae
Akash <akashsunilsingh5555@gmail.com>
2023-03-12 23:35:05 +0530

    add themm

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t*j%5ct68cou#7=l61fkdz=x7dgo9sikg3c20jq%lc!gn!k!+5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['notesplus.azurewebsites.net','127.0.0.1']
 
