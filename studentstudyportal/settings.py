27b0de8ebecc5712385e2dd65eb301d08a4c33ab
Akash <akashsunilsingh5555@gmail.com>
2023-03-13 19:59:18 +0530

    add it

--- studentstudyportal/settings.py
+++ studentstudyportal/settings.py
@@ -150,6 +150,6 @@ AZURE_ACCOUNT_NAME = 'notesplusstorage'
 AZURE_ACCOUNT_KEY = 'ReX2oIpu7uoz+wAkcVN3R11ucK0Q0aHvjxjVSfiytqBaSUyIh+lD5OyJ8dD3rq8y4Pr6AzLi6nkD+AStdnSE8w=='
 AZURE_CONTAINER = 'notesplusss'
 
-
+CSRF_TRUSTED_ORIGINS = ["https://notesplus.azurewebsites.net/", "https://notesplus.azurewebsites.net/"]
 
 
